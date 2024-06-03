45507bad0667a365cd850f96f6041e56118d8280
AlinaGay <gayfullina.a@gmail.com>
2024-06-03 12:22:21 +0200

    add dynamic path segments

--- my_page/challenges/views.py
+++ my_page/challenges/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-
 def january(request):
-  return HttpResponse("It is January")
+  return HttpResponse("Eat no meet for the entire month!")
 
 def february(request):
-  return HttpResponse("It is February!")
+  return HttpResponse("Walk for at least 20 minutes every day!")
+
+def march(request):
+  return HttpResponse("Learn Django for at least 20 minutes every day!")
+
+def monthly_challenge(request, month):
+  challenge_text = None
+  if month == "january":
+    challenge_text = "Eat no meet for the entire month!"
+  elif month == "february":
+    challenge_text = "Walk for at least 20 minutes every day!"
+  elif month == "march":
+    challenge_text = "Learn Django for at least 20 minutes every day!"
+  else:
+    return HttpResponseNotFound("This month is not supported!")    
+  return HttpResponse(challenge_text)
