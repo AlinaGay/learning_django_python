4a8f6bee961f75d7c961453ec79cb0fec5aff685
AlinaGay <gayfullina.a@gmail.com>
2024-06-05 09:10:49 +0200

    add reverse function for urls

--- my_page/challenges/views.py
+++ my_page/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
   "january":"Eat no meet for the entire month!",
@@ -18,8 +19,13 @@ monthly_challenges = {
 # Create your views here.
 def monthly_challenge_by_number(request, month):
   months = list(monthly_challenges.keys())
+
+  if month > len(months):
+    return HttpResponseNotFound("Invalid month")
+  
   redirect_month = months[month - 1]
-  return HttpResponseRedirect("/challenges/" + redirect_month)
+  redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/month
+  return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
   try:
